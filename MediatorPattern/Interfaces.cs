06e3557a9bdeafe49f73e38673802897060ed45a
Nikhil <nikhs.j@gmail.com>
2021-08-15 23:12:53 +0530

    Add project files.

---
+++ MediatorPattern/Interfaces.cs
@@ -0,0 +1,16 @@
+﻿namespace MediatorPattern
+{
+    public interface IRequest<out TResponse>
+    {
+    }
+
+    public interface IHandler<in TRequest, out TResponse> where TRequest:IRequest<TResponse>
+    {
+        TResponse Handle(TRequest request);
+    }
+
+    public interface IMediator
+    {
+        T Send<T>(IRequest<T> request);
+    }
+}
